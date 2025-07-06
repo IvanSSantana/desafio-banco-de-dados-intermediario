9b7de28667f2e10fbef2ca0f52c3f67bd8ee958d
IvanSSantana <ivansantospriv@gmail.com>
2025-07-05 23:31:41 -0300

    commit 5 - Finalizando todas estruturações e adicionando segunda solução para o terceiro enunciado

--- main.sql
+++ main.sql
@@ -22,14 +22,14 @@ CREATE TABLE Produto_Estoque (
 -- Inserindo valores não desejados na query 2
 INSERT INTO Produto(nome_produto) VALUES ('Máquina de datilografia 5500x');
 INSERT INTO Produto_Categoria(id_produto, id_categoria, categoria)
-VALUES (1, 1, 'Eletrônicos')
-INSERT INTO Produto_Estoque(id_produto, quantidade) VALUES (1, 24)
+VALUES (1, 1, 'Eletrônicos');
+INSERT INTO Produto_Estoque(id_produto, quantidade) VALUES (1, 24);
 
 -- Inserindo valores requisitados na query 2
 INSERT INTO Produto(nome_produto) VALUES ('Camiseta Vulcânica');
 INSERT INTO Produto_Categoria(id_produto, id_categoria, categoria)
-VALUES (2, 2, 'Roupas')
-INSERT INTO Produto_Estoque(id_produto, quantidade) VALUES (2, 100)
+VALUES (2, 2, 'Roupas');
+INSERT INTO Produto_Estoque(id_produto, quantidade) VALUES (2, 100);
 
 -- QUERY 1
 SELECT P.nome_produto, PC.categoria, PE.quantidade
@@ -44,24 +44,26 @@ DELETE FROM Produto
           FROM Produto P
             INNER JOIN Produto_Categoria PC ON P.id_produto = PC.id_produto
         WHERE PC.categoria = 'Roupas'
-)
+);
 
--- QUERY 3
-/* CREATE TABLE Clientes (
+-- QUERY 3 (Solução 1: fatiamento de strings)
+-- Estruturação da tabela
+CREATE TABLE Clientes (
     id_cliente INT IDENTITY PRIMARY KEY,
     nome VARCHAR(100),
     genero VARCHAR(1) -- M ou F
-) 
+);
 
 INSERT into Clientes (nome, genero) VALUES ('Pedro Velho Teixeira', 'M');
 INSERT into Clientes (nome, genero) VALUES ('Olívia Palito Barroca', 'F');
-INSERT into Clientes (nome) VALUES ('Itamar Fonseca Oliveira'); */
+INSERT into Clientes (nome) VALUES ('Itamar Fonseca Oliveira');
 
+-- O solicitado pelo enunciado
 ALTER TABLE Clientes
-ADD Nome_Completo VARCHAR(110);
+ADD nome_completo VARCHAR(110);
 
 UPDATE Clientes
-SET Nome_Completo =  
+SET nome_completo =  
 (CASE genero
   WHEN 'M' THEN -- Homem
     CONCAT_WS(' ',
@@ -85,6 +87,64 @@ ELSE -- Caso não tenha gênero, não terá título
     SUBSTRING(nome, (PATINDEX('% %', nome) + 1), 1), 
     SUBSTRING(SUBSTRING(nome, PATINDEX('% %', nome)+1),
       PATINDEX('% %', SUBSTRING(nome, PATINDEX('% %', nome)+1))+1))
-END)
+END);
 
-SELECT * FROM Clientes
+SELECT * FROM Clientes;
+DROP TABLE Clientes;
+
+-- QUERY 3 (Solução 2: concatenando campos)
+-- Estruturação da entidade
+CREATE TABLE Clientes (
+    id_cliente INT IDENTITY PRIMARY KEY,
+    primeiro_nome VARCHAR(50),
+    meio_nome VARCHAR(100),
+    ultimo_nome VARCHAR(50),
+    genero VARCHAR(1) -- M ou F
+);
+
+INSERT INTO Clientes (primeiro_nome, meio_nome, ultimo_nome, genero)
+  VALUES ('Pedro', 'Augusto', 'Teixeira', 'M');
+INSERT INTO Clientes (primeiro_nome, meio_nome, ultimo_nome, genero)
+  VALUES ('Maria', 'Palito', 'Pereira', 'F');
+INSERT INTO Clientes (primeiro_nome, meio_nome, ultimo_nome)
+  VALUES ('Itamar', 'Fonseca', 'Castro');
+INSERT INTO Clientes (primeiro_nome, ultimo_nome)
+  VALUES ('Marcos', 'Peixe');
+INSERT INTO Clientes ()
+  VALUES ();
+
+-- Requisitado pelo enunciado
+ALTER TABLE Clientes
+ADD nome_completo VARCHAR(200);
+
+SELECT
+CASE genero
+  WHEN 'M' THEN
+    CONCAT_WS(' ', 'Sr.', primeiro_nome, 
+    SUBSTRING(meio_nome, 1, 1), ultimo_nome)
+  WHEN 'F' THEN
+    CONCAT_WS(' ', 'Sra.', primeiro_nome, 
+    SUBSTRING(meio_nome, 1, 1), ultimo_nome)
+  ELSE
+    CONCAT_WS(' ', primeiro_nome, 
+    SUBSTRING(meio_nome, 1, 1), ultimo_nome)
+END
+-- Dessa maneira automicamente se adapta a campos nulos
+FROM Clientes;
+
+UPDATE Clientes
+SET nome_completo =
+(CASE genero
+  WHEN 'M' THEN
+    CONCAT_WS(' ', 'Sr.', primeiro_nome, 
+    SUBSTRING(meio_nome, 1, 1), ultimo_nome)
+  WHEN 'F' THEN
+    CONCAT_WS(' ', 'Sra.', primeiro_nome, 
+    SUBSTRING(meio_nome, 1, 1), ultimo_nome)
+  ELSE
+    CONCAT_WS(' ', primeiro_nome, 
+    SUBSTRING(meio_nome, 1, 1), ultimo_nome)
+END);
+
+SELECT * FROM Clientes;
+
